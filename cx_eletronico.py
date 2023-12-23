cb556240771c652b2a18f1e608b55d15148bba9c
Patrick Avancini Cegatta <136925471+cegattaap@users.noreply.github.com>
2023-12-22 22:18:21 -0300

    Add files via upload

---
+++ cx_eletronico.py
@@ -0,0 +1,21 @@
+print('{:-^30}'.format(' Caixa eletrônico '))
+saque = int(input('Valor de saque: R$'))
+total = saque
+cedula = 50
+totalcedula = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        totalcedula += 1
+    else:
+        if totalcedula > 0:
+            print(f'Total de {totalcedula} cédulas de R${cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totalcedula = 0
+        if total == 0:
+            break
